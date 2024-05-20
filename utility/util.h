dbaa7148e60e55a0868c428adf63c3c59f4c9e80
michaelxuuu <xyjmichael@gmail.com>
2024-05-20 14:27:08 -0700

    nothing much

--- utility/util.h
+++ utility/util.h
@@ -1,11 +1,6 @@
-#ifndef _util_h_
-#define _util_h_
-
 void *memcpy(void *dest, void *src, int n);
 void *memset(void *p, int c, int len);
 int strcmp(char *s1, char *s2);
 int strnlen(char *s, int n) ;
 int strlen(char *s);
 char *strncpy(char *dest, char *src, int n);
-
-#endif
