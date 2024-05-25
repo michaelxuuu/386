c0c4363b5e59e07cc2bf58192cbb91fc9dc0d911
michaelxuuu <xyjmichael@gmail.com>
2024-05-24 18:23:17 -0700

    nothing much

--- grab/pio.c
+++ grab/pio.c
@@ -24,3 +24,21 @@ void outl(uint32_t dword, uint16_t port) {
     asm ("outl %0, %1" : : "eax"(dword), "d"(port));
 }
 
+// INTEL 80386 PROGRAMMER'S REFERENCE MANUAL 1986
+// 8.2.2 Block I/O Instructions
+void insl(uint16_t port, void *dest, int cnt)
+{
+  asm volatile("cld; rep insl" :
+               "=D" (dest), "=c" (cnt) :
+               "d" (port), "0" (dest), "1" (cnt) : // "0" and "1" tells the compiler "=D" and "=c" are also output registers (meaning they are modified by this instruction!)
+               "memory", "cc");
+}
+
+void outsl(void *src, uint16_t port, int cnt)
+{
+  asm volatile("cld; rep outsl" :
+               "=S" (src), "=c" (cnt) :
+               "d" (port), "0" (src), "1" (cnt) :
+               "cc");
+}
+
